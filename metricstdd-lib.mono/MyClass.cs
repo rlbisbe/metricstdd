42a7593448b13245782bbec0df3c6197c93d72ea
rlbisbe <rober90@gmail.com>
2013-10-28 19:46:21 +0100

    Added mono solution

---
+++ metricstdd-lib.mono/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace metricstddlib.mono
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ metricstdd-tests.mono/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace metricstddtests.mono
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
